e6737105a1e7bc86b6d3c7619020710ea0c622c7
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2023-12-28 19:25:20 -0800

    made it to the for each loop part, and got stuck, looking for solutions

--- public/index.js
+++ public/index.js
@@ -1,8 +1,78 @@
+
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+    // const apiKey = '5fbf5cf7f9174086bb0a56b64b9d2f58';
+    // const symbols = ['GME','MSFT','DIS','BNTX'];
+    // const apiUrl = 'https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=your_api_key'
+
+
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
+
+
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=  GME,MSFT,DIS,BTNX&interval=1min&apikey=5fbf5cf7f9174086bb0a56b64b9d2f58')
+    let result = await response.json()
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BTNX = result.BTNX
+
+    const stocks = [GME, MSFT, DIS, BTNX];
+    console.log(result)
+       
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+
+    
+
+    stocks.forEach( stock => stock.values.reverse())
+
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    
+    
+    
+    
+    
+    console.log(stocks[0].values)
+    
+
+    
+    
+                                                   
+
 
 }
 
