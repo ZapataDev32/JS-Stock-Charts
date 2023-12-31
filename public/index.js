fa43a99f714f182b6c47a6565ea3579709a7348e
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2023-12-30 18:04:03 -0800

    project all done, all 3 charts are displayed

--- public/index.js
+++ public/index.js
@@ -1,33 +1,28 @@
-
+import mockData from './mockData.js'
 
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+    
     // const apiKey = '5fbf5cf7f9174086bb0a56b64b9d2f58';
     // const symbols = ['GME','MSFT','DIS','BNTX'];
     // const apiUrl = 'https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=your_api_key'
 
 
 
-// Bonus Note: 
-// Another way to write the above lines would to refactor it as:
-   // const {GME, MSFT, DIS, BTNX} = result 
-// This is an example of "destructuring" an object
-// "Destructuring" creates new variables from an object or an array
-
-
-    let response = await fetch('https://api.twelvedata.com/time_series?symbol=  GME,MSFT,DIS,BTNX&interval=1min&apikey=5fbf5cf7f9174086bb0a56b64b9d2f58')
-    let result = await response.json()
+    // let response = await fetch('https://api.twelvedata.com/time_series?symbol=  GME,MSFT,DIS,BTNX&interval=1min&apikey=5fbf5cf7f9174086bb0a56b64b9d2f58')
+    // let result = await response.json()
+    // console.log(result)
+    const result = mockData
+    console.log(result)
     let GME = result.GME
     let MSFT = result.MSFT
     let DIS = result.DIS
-    let BTNX = result.BTNX
+    let BNTX = result.BNTX
 
-    const stocks = [GME, MSFT, DIS, BTNX];
-    console.log(result)
+    const stocks = [GME, MSFT, DIS, BNTX];
        
     function getColor(stock){
         if(stock === "GME"){
@@ -46,31 +41,88 @@ async function main() {
 
     
 
-    stocks.forEach( stock => stock.values.reverse())
-
+    stocks.forEach(stock => {
+        if (stock.values) {
+            stock.values.reverse();
+        }
+    });
+    console.log(stocks)
     // Time Chart
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: stocks[0].values.reverse().map(value => value.datetime),
-            datasets: stocks.map(stock => ({
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
                 label: stock.meta.symbol,
-                data: stock.values.reverse().map(value => parseFloat(value.high)),
-                backgroundColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
                 borderColor: getColor(stock.meta.symbol),
             }))
+        }
+    });
+
+    // Bar chart
+
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
+        }
+    });
+    
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    calculateAverage(stock.values)
+                ))
+            }]
         }
     });
     
     
     
     
-    
     console.log(stocks[0].values)
     
-
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
+}
     
-    
+function calculateAverage(values) {
+    let total = 0;
+    values.forEach(value => {
+        total += parseFloat(value.high)
+    })
+    return total / values.length
+}
                                                    
 
 
